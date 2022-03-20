8856db278cd45de8001285d720d40098f83d0b18
cristi.dbr <cristian.dobre@rinftech.com>
2022-03-20 22:37:00 +0200

    Adds setup py

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""Setuptools entry point."""
+import codecs
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+dirname = os.path.dirname(__file__)
+readme_filename = os.path.join(dirname, 'README.rst')
+
+description = 'Node-like Websockets for Python'
+long_description = description
+if os.path.exists(readme_filename):
+    readme_content = codecs.open(readme_filename, encoding='utf-8').read()
+    long_description = readme_content
+
+setup(name='nwebsocket',
+      version='0.9.0',
+      description=description,
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      url='https://github.com/cristidbr-adapta/nwebsocket',
+      author='Cristian Dobre',
+      author_email='cristian.dobre@adaptarobotics.com',
+      license='MIT',
+      packages=['nwebsocket'],
+      install_requires=[
+          'curio',
+          'wsproto'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
