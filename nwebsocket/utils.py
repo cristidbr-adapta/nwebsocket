d4f0e79334bf5efe1a4b97c3bc0d6d9e7199ccfc
cristi.dbr <cristian.dobre@rinftech.com>
2022-03-23 20:30:58 +0200

    Adds test for URI parsing

--- nwebsocket/utils.py
+++ nwebsocket/utils.py
@@ -8,7 +8,7 @@ URL Parsing utilities.
 from urllib.parse import urlparse
 
 
-def uriparse(uri, default_port=8080):
+def uriparse(uri, default_port=None):
     info = urlparse(uri)
     host = info.netloc.split(':')[0]
     path = info.path
@@ -16,6 +16,9 @@ def uriparse(uri, default_port=8080):
     try:
         port = int(info.netloc.split(':')[-1])
     except BaseException:
-        port = default_port
+        if default_port is None:
+            port = 443 if info.scheme == 'wss' else 80 
+        else:
+            port = default_port
 
     return dict(port=port, path=path, host=host)
