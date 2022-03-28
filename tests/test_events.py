393c80415df096b031dd74d041e0a90c51feddb2
cristi.dbr <cristian.dobre@rinftech.com>
2022-03-28 20:12:41 +0300

    Removes asyncio marks

--- tests/test_events.py
+++ tests/test_events.py
@@ -5,11 +5,9 @@ import pytest
 from nwebsocket.events import ws_events_manage, ws_socket_manage
 
 
-@pytest.mark.asyncio
 async def test_events_manage():
     pass
 
 
-@pytest.mark.asyncio
 async def test_socket_manage():
     pass
